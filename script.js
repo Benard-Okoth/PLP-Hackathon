9c9ed0bfa0e972373a001840ceab0a079bb509e1
Benard-Okoth <abwaobenard1@gmail.com>
2025-04-04 21:53:22 +0300

    Added files

---
+++ script.js
@@ -0,0 +1,9 @@
+let currentIndex = 0;
+
+function moveSlide(step) {
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
+    const slider = document.querySelector(".slider");
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
